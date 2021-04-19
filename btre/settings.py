c7eb667ec154e3ac0d1c4ab48ad7d9099df80a34
Gisa Kaze Fredson <fredson.coder.com>
2021-04-19 11:23:56 +0200

    Installing postgresql and creating models

--- btre/settings.py
+++ btre/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'fredson45@800G',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+# Mdedia Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
