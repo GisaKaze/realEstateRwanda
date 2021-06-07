9a6e6af623a2d709abf36bd23357164285b9340f
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-07 18:48:30 +0200

    Add filter, search(not working well), display link and editable publish type

--- listings/admin.py
+++ listings/admin.py
@@ -4,6 +4,10 @@ from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'address', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
 
 admin.site.register(Listing, ListingAdmin)
 # admin.site.register(Realtor)
