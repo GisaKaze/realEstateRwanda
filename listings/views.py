e596102f224c22b3fe193a0dcbb52ce1fdab5604
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-08 21:53:49 +0200

    Add dynamic listings.....

--- listings/views.py
+++ listings/views.py
@@ -13,7 +13,7 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
