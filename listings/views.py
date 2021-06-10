271e10c16fbf859e88f643466e3c8011946a9cc6
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-10 11:29:46 +0200

    Add filter, search(not working well), display link and editable publish type

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    
+    context = {
+        'listings': listings
+    }
+
+    
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
