e78bc8a87fb5a4527ae5f734c311935488082b07
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-11 23:39:13 +0200

    Add listing only published properties....

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     
     paginator = Paginator(listings, 6)
     page =  request.GET.get('page')
