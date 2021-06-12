d391ea820a8416137200e0447eac9ae2c7326e15
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-12 19:38:10 +0200

    working.......

--- pages/views.py
+++ pages/views.py
@@ -4,13 +4,13 @@ from listings.models import Listing
 
 
 def index(request):
-    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     
     context = {
         'listings': listings
     }
     
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
