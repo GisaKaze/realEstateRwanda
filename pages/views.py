27b851ca39ab5860ba680ec027d31fa7adb2e0f0
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-12 13:08:33 +0200

    working.......

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 
 
 def index(request):
+    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    
+    context = {
+        'listings': listings
+    }
+    
     return render(request, 'pages/index.html')
 
 
