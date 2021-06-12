745498f9fda01b1de95f6d9af6be816103cd7920
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-06-12 22:24:52 +0200

    Add realtors view listing them

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from listings.models import Listing
 
+from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -14,4 +15,15 @@ def index(request):
 
 
 def about(request):
+    # Get all Realtor
+    realtors = Realtor.objects.order_by('-hire_date')
+    
+    # Get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    
     return render(request, 'pages/about.html')
