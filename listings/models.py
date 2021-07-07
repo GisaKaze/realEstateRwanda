ef29bff39e8367cb116a9f0015340118cf47cc80
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-07-07 22:41:34 +0200

    Search completed

--- listings/models.py
+++ listings/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
+
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=20)
@@ -21,9 +22,10 @@ class Listing(models.Model):
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_5= models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_6= models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
+
     def __str__(self):
         return self.title
--- listings/views.py
+++ listings/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, district_choices
 
 from .models import Listing
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     
     paginator = Paginator(listings, 6)
-    page =  request.GET.get('page') 
+    page =  request.GET.get('page')
     paged_listings = paginator.get_page(page)
     
     context = {
         'listings': paged_listings
     }
-
-    
     return render(request, 'listings/listings.html', context)
 
 
@@ -28,4 +28,42 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains = keywords)
+    
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact = city)
+            
+    # District
+    if 'district' in request.GET:
+        district = request.GET['district']
+        if district:
+            queryset_list = queryset_list.filter(district__iexact = district)
+    
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte = bedrooms)
+            
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte = price)
+    context = {
+        'district_choices': district_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
