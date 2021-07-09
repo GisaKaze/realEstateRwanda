63e34ffe774c5c37654fba8fe191f981fee44d2e
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-07-09 21:56:11 +0200

    Accounts initiated

---
+++ listings/choices.py
@@ -0,0 +1,42 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': '100000',
+    '200000': '200000',
+    '300000': '300000',
+    '400000': '400000',
+    '500000': '500000',
+    '800000': '800000',
+    '1200000': '1200000',
+    '1500000': '1500000',
+    '2000000': '2000000',
+    '10000000': '10M+',
+}
+
+district_choices = {
+    'KCK': 'Kicukiro',
+    'GSB': 'Gasabo',
+    'NYR': 'Nyarugenge',
+    'NYG': 'Nyagatare',
+    'KYZ': 'Kayonza',
+    'RMG': 'Rwamagana',
+    'BUG': 'Bugesera',
+    'HUY': 'Huye',
+    'NYZ': 'Nyanza',
+    'KMY': 'Kamonyi',
+    'RUS': 'Rusizi',
+    'RBV': 'Rubavu',
+    'KRG': 'Karongi',
+    'MSZ': 'Musanze',
+}
