0c751958d441e2c182948f9df242fa335bd211c1
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-07-12 15:02:08 +0200

    Whether form can be submitted

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render, redirect
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # print('SUBMITTED REGISTRATION')
+        # return redirect('register')
+        #Register User
+        return
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
+    if request.method == 'POST':
+        # print('SUBMITTED FOR VERIFICATION')
+        # return redirect('login')
+        #Login User
+        return 
     return render(request, 'accounts/login.html')
 
 
