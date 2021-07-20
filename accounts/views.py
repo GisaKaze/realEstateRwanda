6b14fbd221bb60233650f0e50ad0b216a7934ffb
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-07-20 16:26:42 +0200

    Add User Loggin and dashboard for direction 😎

--- accounts/views.py
+++ accounts/views.py
@@ -42,12 +42,28 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        return 
-    return render(request, 'accounts/login.html')
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Wrong password or email')
+            return redirect('login')
+    
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, "You're logged-out")
+        return redirect('index')
 
 
 def dashboard(request):
