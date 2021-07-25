faa7c28139e24cb7d6c4fa5cc9d1c1a1cf945419
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-07-25 15:15:53 +0200

    Sending email done!

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,11 +14,28 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
         
+        # Check if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'Inquiry is already made')
+                return redirect('/listings/'+listing_id)
+        
         contact = Contact(listing_id=listing_id, listing=listing,
                           name=name, email=email, phone=phone, 
                           message=message, user_id=user_id)
         
         contact.save()
+        
+        # Send email
+        send_mail(
+            'Real Estate Property Inquiry',
+            'There has been an inquiry for ' + listing + ' property.\n\n\nSign into the admin panel for more information',
+            'fredson.coder@gmail.com',
+            [realtor_email, 'azefredson@gmail.com'],
+            fail_silently = False
+        )
         
         messages.success(request, "Your request has been submitted, a realtor will get back to you soon")
         return redirect('/listings/'+listing_id)
--- realEstate/settings.py
+++ realEstate/settings.py
@@ -144,3 +144,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fredson.coder@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
